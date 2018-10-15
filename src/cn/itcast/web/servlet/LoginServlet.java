504dcb115e727f868025c71977187a3bd0dd74fc
GaoXiang <39550837+gx8858@users.noreply.github.com>
2018-10-15 12:29:22 +0800

    登录操作servlet,主要来记住用户名

--- src/cn/itcast/web/servlet/LoginServlet.java
+++ src/cn/itcast/web/servlet/LoginServlet.java
@@ -8,6 +8,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * 登录操作servlet,主要来记住用户名
+ */
 public class LoginServlet extends HttpServlet {
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
